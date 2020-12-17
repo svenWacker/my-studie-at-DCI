71e0772afb98bd92823fcde165b586f9dbdab811
svenWacker <s.u.wacker@outlook.de>
2020-12-17 15:01:46 +0100

    update

---
+++ dec/17-12/coding.js
@@ -0,0 +1,22 @@
+//coding 17.12.20121
+
+let username = "Wacker";
+
+console.log(username);
+console.log(typeof username);
+console.log(username.length);
+console.log(username[1]);
+console.log(username[5]);
+
+let name = "Sven";
+let age = 45;
+let year = 2020;
+console.log(name, "is", age, "in", year);
+console.log("is");
+console.log(age);
+console.log ("in");
+console.log (year);
+
+let isMarried = 0 < 1;
+let programmingLanguage = "JavaScript";
+let isFun = 1;
---
+++ dec/17-12/index.js
@@ -0,0 +1,44 @@
+//Printing out a string
+console.log('Sven');
+console.log('I love Casa');
+//Mathematical expressions: (+,-,*,/,%)
+console.log(1+1);
+console.log(2*2);
+console.log()
+//Boolean expressions: comparison operators (===, !==, >=, <=, >, <)
+console.log(12==2);
+//
+//console.log('Sven');
+let x = 3;
+var name = "Sven";
+let boo = 1 <3;
+console.log(x);
+console.log(name);
+console.log(boo);
+
+let str1 = "Hi ";
+let str2 = "I am Sven"
+console.log(str1 + str2);
+console.log(typeof strSum);
+
+//ternary conditions
+let person = 21;
+console.log(person > 16);
+person > 16 ? console.log("Go Party"): console.log("Stay Home");
+
+let drive = 55;
+drive > 18
+    ? console.log("Yeah lets's drive now")
+    : console.log("Nope sorry");
+//comparison operators (===, !==, >=, <=, >, <)
+console.log(1 == 1);
+let num1 = 2;
+let num2 = "2";
+//console.log(num1 == num2);//comparison number
+console.log(num1 === num2);//comparison type
+//coding 17.12.20121
+
+let string = "wacker";
+console.log(string);
+console.log;
+
