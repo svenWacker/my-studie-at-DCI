c0964ad00f3514732e777bff791846bb88e9fc95
svenWacker <s.u.wacker@outlook.de>
2020-12-17 15:26:44 +0100

    endupdate

--- dec/17-12/coding.js
+++ dec/17-12/coding.js
@@ -1,7 +1,9 @@
 //coding 17.12.20121
 
 let username = "Wacker";
-
+console.log();
+console.log();
+console.log();
 console.log(username);
 console.log(typeof username);
 console.log(username.length);
@@ -12,11 +14,31 @@ let name = "Sven";
 let age = 45;
 let year = 2020;
 console.log(name, "is", age, "in", year);
-console.log("is");
-console.log(age);
-console.log ("in");
-console.log (year);
+//console.log("is");
+//console.log(age);
+//console.log ("in");
+//console.log (year);
 
 let isMarried = 0 < 1;
 let programmingLanguage = "JavaScript";
 let isFun = 1;
+console.log(isFun==1);
+console.log(programmingLanguage[0],programmingLanguage[4]);
+console.log(programmingLanguage.length);
+console.log();
+console.log("Ternary");
+let isStudent = 1;
+isStudent == 1
+    ? console.log("Ticket costs 5,00 €")
+    : console.log("Ticket costs 12,00 €");
+let speedCheck = 50;
+speedCheck <= 50
+    ? console.log("You're going too fast")
+    : console.log("You're driving below the speed limit, Oma");
+
+
+
+
+
+
+
