2aa5cbacb4497733cd9962b23ef95203de446b6a
svenWacker <s.u.wacker@outlook.de>
2021-01-13 10:01:05 +0100

    solutions

--- jan/13-01/solutionAss.js
+++ jan/13-01/solutionAss.js
@@ -1,17 +1,16 @@
-// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
-
-// Examples:
-
-// isLEQZero(3) ➞ false
-// isLEQZero(0) ➞ true
-// isLEQZero(-4) ➞ true
-// isLEQZero(10) ➞ false
 const isLEQZero = (num) => {
-    return num >= 0;
-}
-console.log(isLEQZero(3));
+    return console.log(num <= 0);
+};
+isLEQZero(3);
 isLEQZero(0);
 isLEQZero(-4);
 isLEQZero(10);
 
-const
+const dogAge = (dogYear) => {
+    const humanYear = dogYear * 7;    ;
+    return console.log(`Your doggy is ${humanYear} years old in dog years!`);
+    //return `Your doggy is ${humanYear} years old in dog years!`;
+};
+dogAge(4);
+dogAge(10);
+dogAge(8);
