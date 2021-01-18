9a2473c8e5b8e4e9faf949dc96bf380d54d244c0
svenWacker <s.u.wacker@outlook.de>
2021-01-18 15:13:06 +0100

    assignment

---
+++ jan/18-01/assignment.js
@@ -0,0 +1,96 @@
+// Mark and John are trying to compare their BMI (Body Mass Index),
+// which is calculated using the formula:
+// BMI = mass / (height * height).
+// (mass in kg, i.e. 72.5, and height in metres, i.e. 1.65).
+// 1. Store Mark’s and John’s mass and height in variables.
+const markH = 1.65;
+const markM = 85;
+const johnH = 1.9;
+const johnM = 120;
+
+// 2. Calculate both their BMIs and store their BMIs in variables.
+let markBMI = markM / (markH * markH);
+let johnBMI = johnM / (johnH * johnH);
+console.log(markBMI);
+// 3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+let bool = markBMI > johnH;
+console.log(`Is Mark's BMI higher than JOhn's? Why jes, it's ${bool}`);
+// 4. Print a string to the console containing the variable from step 3 using string interpolation. (e.g. “Is Mark’s BMI higher than John’s? Why yes, it’s [boolean], it is”).
+// 5. Create an if statement which prints the name and BMI of the person with the highest BMI.
+if (markBMI > johnBMI) {
+  console.log("Mark is the winner");
+} else {
+  console.log("John is the winner");
+}
+/*
+const names = ["Mark", "John", "Sven"];
+const height = [1.65, 1.8, 1.74];
+const mass = [72.5, 85, 72];
+const bmi = [2];
+for (let i = 0; i < names.length; i++) {
+  return (bmi = (mass / height) * height);
+}
+console.log(`
+  ${names[i]} is ${height[i]}m tall, weights ${mass[i]} kg
+  BMI: ${bmi}
+  `);
+*/
+
+// Make a variable for firstName and age and give each variable values.
+// Create an if/else statement to check whether the person’s age is less than 13.
+// If so, print “firstName is a child”.
+// If the age is equal to or more than 13 and less than 20,
+// print “firstName is a teenager”.
+// If the person’s age is equal to and more than 20 and less than 30,
+// then print “firstName is a young adult”.
+// If none of these conditions apply, print “firstName is a adult”.
+
+let firstName = "Sven";
+let age = 31;
+
+const lifeStatus = (firstName, age) => {
+  status = "";
+  if (age < 13) {
+    status = "child";
+  } else if (age >= 13 && age < 20) {
+    status = "teenager";
+  } else if (age >= 20 && age < 30) {
+    status = "young adult";
+  } else {
+    status = "adult";
+  }
+  console.log(status);
+  return `${firstName} is a ${status}`;
+};
+//let firstName = "Sven";
+//let age = 31;
+
+console.log(firstName, age);
+
+// Create a program that capitalizes the first letter of each element in an array of names. Examples:
+// [“matt”, “sara”, “lara”] ➞ [“Matt”, “Sara”, “Lara”]
+const names1 = ["matt", "sara", "lara"];
+// [“samuel”, “MARIA”, “luke”, “mary”] ➞ [“Samuel”, “Maria”, “Luke”, “Mary”]
+const names2 = ["samuel", "MARIA", "luke", "mary"];
+// [“Cynthia”, “Karen”, “Jane”, “Carrie”] ➞ [“Cynthia”, “Karen”, “Jane”, “Carrie”]
+const names3 = ["Cynthia", "Karen", "Jane", "Carrie"];
+for (let i = 0; i < names1.length; i++) {
+  console.log(names1[i][0].toUpperCase() + names1[i].slice(1));
+}
+for (let i = 0; i < names2.length; i++) {
+  console.log(names2[i][0].toUpperCase() + names2[i].slice(1).toLowerCase());
+}
+for (let i = 0; i < names3.length; i++) {
+  console.log(names2[i][0].toUpperCase() + names3[i].slice(1));
+}
+// Create an array of city names. Loop through the array and add the city names to a string. Examples:
+// [Berlin, Paris, Prague, Rome] ➞ expected output: “Berlin, Paris, Prague, Rome”.
+const cities = ["Berlin", "Paris", "Prague", "Rome"];
+for (let i = 0; i < cities.length; i++) {
+  console.log(cities[i][0].toUpperCase() + cities[i].slice(1));
+}
+// [Maria, Mike, Paul, Doven] ➞ expected output: “Hello Maria! Hello Mike! Hello Paul! Hello Doven!”
+const names5 = ["Maria", "Mike", "Paul", "Doven"];
+for (let i = 0; i < names5.length; i++) {
+  console.log(`Hello ${names5[i]}! with index: ${i}`);
+}
--- jan/18-01/index.js
+++ jan/18-01/index.js
@@ -1,3 +1,5 @@
+/*
+
 // for loop
 for (let i = 0; i < 5; i++) {
   console.log(i);
@@ -39,4 +41,87 @@ for (let i = 1; i <= 10; i++) {
   for (let j = 1; j <= 10; j++) {
     console.log(`${j} * ${i}\t = ${i * j}`);
   }
+}
+
+console.log("----------------");
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log(`${i} is even`);
+  } else {
+    console.log(`${i} is odd`);
+  }
+}
+
+console.log("----------------");
+for (let i = 0; i <= 4; i++) {
+  console.log("*".repeat(i));
+}
+console.log("----------------");
+let drawShape = "";
+for (let i = 0; i <= 4; i++) {
+  drawShape = "";
+  for (let j = 0; j <= i; j++) {
+    drawShape += "+";
+  }
+  console.log(drawShape);
+  drawShape = "";
+}
+
+const names = ["Sven", "Maria", "Niklas", "Sila"];
+const work = ["WebDev", "Flowerist", "Schoolboy", "Student"];
+for (let i = 0; i < names.length; i++) {
+  console.log(`
+   __________________________
+  |                          
+  | Hi, my name is ${names[i]},
+  | I am a ${work[i]}.
+  |__________________________
+  `);
+}
+*/
+let color = "red";
+switch (color) {
+  case "pink":
+    isLogged = false;
+
+    // more code
+    console.log("cool this is pink");
+    break;
+  case "red":
+    console.log("cool this is red");
+    break;
+  case "white":
+    console.log("cool this is white");
+    break;
+  default:
+    console.log("sorry you need to enter a color");
+}
+// more
+
+let userKidsNumber = 11;
+switch (userKidsNumber) {
+  case 1:
+    console.log("You are new parent with 1 child");
+    break;
+  case 2:
+    console.log("You have 2 kids");
+    break;
+  case 3:
+    console.log("You have 3 kids");
+    break;
+  case 4:
+  case 5:
+  case 6:
+  case 7:
+    console.log("You have many kids");
+    break;
+  case 8:
+  case 9:
+  case 10:
+  case 11:
+  case 12:
+    console.log("You are really happy parents");
+    break;
+  default:
+    console.log("Enter a number between 1 and 12");
 }
