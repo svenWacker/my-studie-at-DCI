8c72e7583cbd41dfccd75361e0c9d88fbdd9ff72
svenWacker <s.u.wacker@outlook.de>
2021-01-20 09:19:56 +0100

    function, counter, password

--- jan/13-01/index.js
+++ jan/13-01/index.js
@@ -22,26 +22,25 @@ Bonus: create another function by doing some changes to the previous function in
   12
   1
 */
-const numbersCountBack = () => { 
+const numbersCountBack = () => {
   let text = "";
-  for(let i = 5; i >=0;i--){
+  for (let i = 5; i >= 0; i--) {
+    // something
+    text = "";
+    for (let j = 1; j <= i; j++) {
       // something
-      text = "";
-      for(let j =1; j<=i; j++){
-            // something
-            text += j;
-        }
-      // something
-      console.log(text);
+      text += j;
     }
+    // something
+    console.log(text);
+  }
 };
 numbersCountBack();
 
 let userData = "";
-const saveData =(userName = "No name", kids =0)=>{
+const saveData = (userName = "No name", kids = 0) => {
   userData = userName;
-
-}
+};
 saveData("Sven");
 console.log(userData);
 
@@ -49,22 +48,23 @@ const countOccurences = (str, letter) => {
   let result = str.split("");
   //console.log(result);
   let counter = 0;
-  for (let i = 0; i < result.length; i++){
+  for (let i = 0; i < result.length; i++) {
     if (result[i] == letter) {
       counter++;
     }
   }
   return counter;
-}
+};
 console.log(countOccurences("This is a string", "i")); // 3
 console.log(countOccurences("xoxoxoxoxoxoxo", "o")); // 7
 console.log(countOccurences("x", "y")); // 0
 
 const passwordGen = () => {
-  let pw ="";
+  let pw = "";
   let pwLength = 30;
-  const pwChar = "abcdefghijklmnopqrstuvwxyzäöüABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜ0123456789!§%&/()=?<>@€$#*+-_°^";
-  for (let i = 0; i <=pwLength; i++) {
+  const pwChar =
+    "abcdefghijklmnopqrstuvwxyzäöüABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜ0123456789!§%&/()=?<>@€$#*+-_°^";
+  for (let i = 0; i <= pwLength; i++) {
     pw += pwChar.charAt(Math.floor(Math.random() * pwChar.length));
   }
   console.log(pw);
