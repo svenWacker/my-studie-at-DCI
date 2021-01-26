3d1f72ea5c9ee7025046721666e5b5fd135e964a
svenWacker <s.u.wacker@outlook.de>
2021-01-26 15:57:37 +0100

    new stuff with map, filter, find + assignments

--- jan/26-01/assignments.js
+++ jan/26-01/assignments.js
@@ -9,7 +9,8 @@ twofer();
 // Write a function  that it still calculates the power
 // of a number even if the exp argument is not passed to it.
 // The default exp should be set to 2.
-const exponent = (x, y = 2) => Math.pow(x, y);
+const exponent = (num, exp = 2) => num ** exp;
+//const exponent = (x, y = 2) => Math.pow(x, y);
 console.log(exponent(3, 3)); // ---> 27
 console.log(exponent(3)); // ---> 9
 
@@ -48,3 +49,62 @@ console.log(average(0)); // ---> 0
 console.log(average(1, 2)); // ---> 1.5
 console.log(average(1, 3, 6, 10)); // ---> 5
 console.log(average(12, 14, 16)); // ---> 14
+
+// hackerSpeak
+//console.log(hackerSpeak("javascript is cool"));
+const hackerSpeak = (str) => {
+  return str
+    .toLowerCase()
+    .split("")
+    .map((chr) => {
+      switch (chr) {
+        case "a":
+          return "4";
+        case "o":
+          return "0";
+        case "e":
+          return "4";
+        case "s":
+          return "4";
+        case "i":
+          return "4";
+        default:
+          return chr;
+      }
+    })
+    .join("");
+};
+console.log(hackerSpeak("javascript is cool"));
+console.log(hackerSpeak("programming is fun"));
+console.log(hackerSpeak("become a coder"));
+console.log();
+
+const hackerSpeak2 = (statement) => {
+  const leet = { a: 4, b: 3, i: 1, o: 0, s: 5 };
+  const speak = (letter) =>
+    leet.hasOwnProperty(letter) ? leet[letter] : letter;
+
+  return statement.split("").map(speak).join("");
+};
+console.log(hackerSpeak2("javascript is cool"));
+
+// Odds and Evens.
+// Create a program that changes a given array by adding 1 to each odd integer and subtracting 1 from each even integer. Examples:
+// [3, 5, 2, 4] ➞ expected output: [4, 6, 1, 3]
+// [6, 9, 10, 20] ➞ expected output: [5, 10, 9, 19]
+
+//console.log(oddOrEven(3, 5, 2, 4));
+const oddOrEven = (...args) => {
+  return args.map((item) => (item % 2 == 0 ? ++item : --item));
+};
+console.log(oddOrEven(3, 5, 2, 4));
+// how many chr in a str(not case sens)
+
+// howManyStr(“I like pizza”,“p”) ->
+const howManyStr = (str, chr) => {
+  return str
+    .toLowerCase()
+    .split("")
+    .filter((item) => item == chr).length;
+};
+console.log(howManyStr("I like pizza", "p"));
--- jan/26-01/index.js
+++ jan/26-01/index.js
@@ -38,12 +38,93 @@ printFunThingsTodo("Sven", "coding", "cooking", "cycling");
 (function printMyName(x, y) {
   console.log(`Hi I am a cool function ${x + y}`);
 })(2, 3);
+
+const books = ["one Day", "one more thing", "Done by me"];
 // .map
 // Converting an array to a different array: `Array.prototype.map(<function>)`
+//const printBook = books.map((item, index) => `${index}. title ${item}`);
+const printLabel = (item, index) => `${index}. title ${item}`;
+const printBook = books.map(printLabel);
+console.log(printBook);
+//console.log(books);
+const numbs = ["1", "2", "3"];
+const nums = numbs.map((item) => parseInt(item));
+console.log(nums);
+
+// .reduce
+// Getting a in single output value
+const reducer = (acc, cur) => acc + cur;
+const resultArr = [2, 3, 1, 3].reduce(reducer); // .reduce(reducer, startingValue)
+console.log(resultArr);
 
 // .filter
 // - Getting an array with items that aline with your condition: `Array.prototype.filter(<function>)`
+const names = ["Sven", "Maria", "Niklas", "Sila"];
+const filArr = names.filter((name) => name.length > 4);
+console.log(filArr);
 
 // .find
 // - Getting a the first item that aline with your condition : `Array.prototype.filter(<function>)`
+const coolNumbers = [1, 2, 11, 3, 4, 5, 6, 7];
+const biggerThanFive = coolNumbers.find((num) => num > 3);
+console.log(biggerThanFive);
+
 //  Maybe more 😏
+// sumNumbers
+// Write a function which accepts any amount of numbers
+// and return the sum of their addition
+const sumNumbers = (...args) => {
+  return args.reduce((acc, cur) => acc + cur);
+  //   return args.reduce((acc, cur) => {
+  //     return acc + cur;
+  //   });
+};
+console.log(sumNumbers(1)); // ---> 1
+console.log(sumNumbers(1, 15)); // ---> 16
+console.log(sumNumbers(25, 25, 20)); // ---> 70
+
+// toCamelCase
+
+const toCamelCase = (str) => {
+  // cool_fun_stuff ->coolFunStuff
+  let strToArr = str.toLowerCase().split("_");
+  let result = strToArr.map((item, i) =>
+    i < 1 ? item : item[0].toUpperCase() + item.slice(1)
+  );
+  return result.join("");
+};
+console.log(toCamelCase("cool_fun_stuff"));
+// hackerSpeak
+
+const hackerSpeak = (str) => {
+  let strToArr = str.split();
+  let result = strToArr.map((item, i) => {
+    i == "a"
+      ? item
+      : (item[i] =
+          "4" || i == "i"
+            ? item
+            : (item[i] =
+                "1" || i == "o"
+                  ? item
+                  : (item[i] =
+                      "0" || i == "e"
+                        ? item
+                        : (item[i] =
+                            "3" || i == "s" ? item : (item[i] = "5")))));
+  });
+  return result.join();
+};
+console.log(hackerSpeak("javascript is cool"));
+console.log(hackerSpeak("programming is fun"));
+console.log(hackerSpeak("become a coder"));
+console.log();
+
+const hackerSpeak2 = (statement) => {
+  const leet = { a: 4, b: 3, i: 1, o: 0, s: 5 };
+  const speak = (letter) =>
+    leet.hasOwnProperty(letter) ? leet[letter] : letter;
+
+  return statement.split("").map(speak).join("");
+};
+console.log(hackerSpeak2("javascript is cool"));
