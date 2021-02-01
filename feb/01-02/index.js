db962e85b9afa712bf4aef2c027ba95566db6043
svenWacker <s.u.wacker@outlook.de>
2021-02-01 12:34:39 +0100

    retireSave

--- feb/01-02/index.js
+++ feb/01-02/index.js
@@ -44,6 +44,7 @@ number = 1;
 console.log(fun());
 
 // self invoked function
+// anonymous
 // invoking == calling
 // functions that return other function
 
@@ -57,3 +58,100 @@ const add = (() => {
 console.log(add());
 console.log(add());
 console.log(add());
+// const add2 = (() => {})();
+
+// SIAF
+// self-invoking anonymous function
+(function () {
+  console.log(`Hi`);
+})();
+
+(() => {
+  console.log(`Hi`);
+})();
+
+// IIAE
+// immediately-invoking anonymous expression
+const sum = ((x, y) => {
+  return x + y;
+})(2, 3);
+console.log(sum);
+
+// Write a function add that uses a closure that performs addition on arguments
+// in two separate function calls
+// add(2)(3)
+
+const add2 = (a) => {
+  return (b) => {
+    return a + b;
+  };
+};
+console.log(add2(2)(3));
+
+// Write a function multiplier that uses a closure to perform multiplication.
+// However, the outer function should be stored in a variable which is then called
+function multiplier(num) {
+  // outer function
+  return (x) => x * num; //inner function
+}
+const times = multiplier(2)(5);
+console.log(times);
+
+(() => {})();
+var all = 3;
+(() => {
+  let all = `hey`;
+  console.log(all);
+})();
+console.log(all);
+
+// Write a function that accepts several parameters and calculates the number of money
+// that will have been saved as pension until a person retires.
+// The function should be self invoked and should have the following parameters:
+
+// The current age of the person // e.g. 40
+// The retirement age of the person // e.g. 60
+// The monthly wage the person earns // e.g. 1000
+// The percentage (as integer) that the person saves each month // e.g. 10%
+
+// If the person has already retired then the message ‘You’re already retired!’
+// should be printed.
+
+// If not, then the program should calculate how many years remain until the person retires,
+// the monthly income and take a specific percent of this income every month as saved money.
+
+// Example:
+// A lady is 40 years old, she retires at 65,
+// she earns $2000 per month and she saves the 5% of it.
+// How much money will she have saved until she retires?
+// Output: $30000
+
+const retireCalc = (currentAge, retireAge, monthWage, percent) => {
+  if (currentAge >= retireAge) {
+    console.log(`You’re already retired!`);
+  } else {
+    let yearsLeft = retireAge - currentAge;
+    let monthLeft = yearsLeft * 12;
+    let monthSave = (monthWage * percent) / 100;
+    let totalSave = monthSave * monthLeft;
+    console.log(
+      `years to work: ${yearsLeft}, month to work: ${monthLeft}, your savings: ${totalSave}`
+    );
+  }
+};
+(() => {})();
+retireCalc(45, 60, 10000, 20);
+
+((currentAge, retireAge, monthlyWage, percent) => {
+  if (currentAge >= retireAge) {
+    console.log(`You’re already retired!`);
+  } else {
+    let yearsToSave = retireAge - currentAge;
+    console.log(`years to work and save: ${yearsToSave}`);
+    let monthsToSave = yearsToSave * 12;
+    console.log(`Month to work: ${monthsToSave}`);
+    let amountSavedByMonth = (monthlyWage * percent) / 100;
+    let totalSave = amountSavedByMonth * monthsToSave;
+    console.log(`Your savings: ${totalSave}`);
+  }
+})(45, 60, 10000, 10);
