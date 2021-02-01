69ec3e408495ee047dce49330b0be19cf4733e1d
svenWacker <s.u.wacker@outlook.de>
2021-02-01 09:57:36 +0100

    self invoked function

--- feb/01-02/index.js
+++ feb/01-02/index.js
@@ -17,3 +17,43 @@ function main(num1, num2) {
 
 console.log(main(2, 3));
 console.log(main(3, 4));
+
+// Nested copes: Accessing outer global variables
+
+let nice = true;
+const outerFunction = (x, y) => {
+  let big = true;
+  console.log(big + " outerFunction");
+  const innerFunction = (num) => {
+    big = false;
+    nice = false;
+    console.log(big + " innerFunction");
+    return num + 10;
+  };
+  return innerFunction(x) + innerFunction(y);
+};
+console.log(outerFunction(3, 4));
+
+// Closure
+let number = 19;
+function fun() {
+  return number + number;
+}
+console.log(fun());
+number = 1;
+console.log(fun());
+
+// self invoked function
+// invoking == calling
+// functions that return other function
+
+const add = (() => {
+  let counter = 0;
+  return () => {
+    counter++;
+    return counter;
+  };
+})();
+console.log(add());
+console.log(add());
+console.log(add());
