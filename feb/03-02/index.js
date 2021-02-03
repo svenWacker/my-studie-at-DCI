fa915b5df23ab2ec5c03a99864a304125aa0cf83
svenWacker <s.u.wacker@outlook.de>
2021-02-03 14:59:26 +0100

    freeCodeCamp day

--- feb/03-02/index.js
+++ feb/03-02/index.js
@@ -250,3 +250,25 @@ var myDog = {
 };
 myDog.name = "Happy Coder";
 console.log(myDog.name);
+myDog.bark = "woof";
+delete myDog.tails;
+console.log(myDog);
+
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+  var lookup = {
+    alpha: "Adams",
+    bravo: "Boston",
+    charlie: "Chicago",
+    delta: "Denver",
+    echo: "Easy",
+    foxtrot: "Frank",
+  };
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}
+console.log(phoneticLookup("charlie"));
