501f683f0339d68ea2b3b2d60427dc8bc64ff169
svenWacker <s.u.wacker@outlook.de>
2021-02-03 15:18:29 +0100

    freeCodeCamp day

--- feb/03-02/index.js
+++ feb/03-02/index.js
@@ -302,3 +302,21 @@ var myMusic = [
     gold: true,
   },
 ];
+// Access the myStorage object and assign the contents of the glove box property
+// to the gloveBoxContents variable. Use dot notation for all properties where possible,
+// otherwise use bracket notation.
+// Setup
+var myStorage = {
+  car: {
+    inside: {
+      "glove box": "maps",
+      "passenger seat": "crumbs",
+    },
+    outside: {
+      trunk: "jack",
+    },
+  },
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
+console.log(gloveBoxContents);
