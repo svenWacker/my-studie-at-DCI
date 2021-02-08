d51db567158de51923c3f959374dac75f007ef62
svenWacker <s.u.wacker@outlook.de>
2021-02-08 09:25:21 +0100

    callbacks, array methods

--- feb/08-02/index.js
+++ feb/08-02/index.js
@@ -1,12 +1,19 @@
 // callbacks
 function first(cb) {
+  const funFunction = () => "Noooo"; // function isn't called
   return cb;
 }
 function cb() {
   return "fun";
 }
-console.log(first(cb));
+
+console.log(first(cb()));
+
 // map
+const names = ["Zain", "Olga", "Nancy"];
+// const heyNames = names.map((item, index) => `Hi ${item}, you live in ${index}`);
+const heyNames = names.map((name) => `Hi ${name}`);
+console.log(heyNames);
 
 // reduce
 
