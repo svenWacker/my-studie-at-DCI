5354408c8bdc185bc8a532d7b80672ac862e2548
svenWacker <s.u.wacker@outlook.de>
2021-02-08 09:08:55 +0100

    callbacks

---
+++ feb/08-02/index.js
@@ -0,0 +1,19 @@
+// callbacks
+function first(cb) {
+  return cb;
+}
+function cb() {
+  return "fun";
+}
+console.log(first(cb));
+// map
+
+// reduce
+
+// filter
+
+// find
+
+// for in
+
+// for of
