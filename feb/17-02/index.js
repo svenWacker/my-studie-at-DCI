fb918cff2d82037846bfa41b4ac46282381f4510
svenWacker <s.u.wacker@outlook.de>
2021-02-17 09:05:15 +0100

    errors

---
+++ feb/17-02/index.js
@@ -0,0 +1,19 @@
+// 20210217
+// 3 types of errors (compilation aka compiler fails to compile, runtime, logic)
+// try catch will find the runtime errors but not the parse errors like typo or syntax problems
+
+// most of the error are coming from other places like working with data
+// `try { ... } catch (e) { ... }`
+
+// try --> to test a block of code
+// catch --> to handel the error
+// throw --> to create a custom errors
+// finally --> to execute a code after try regardless of the result
+
+// We will learn after HTML
+//
+// callbacks
+// Promise
+// async/await
+// RxJs
+// and a lot more 😎
