dd61b6269e43dcf36d8488ffbfe95e2b4eb55eca
svenWacker <s.u.wacker@outlook.de>
2021-04-22 09:56:01 +0200

    more DOM - node tree

--- april/22-04/index.js
+++ april/22-04/index.js
@@ -10,3 +10,35 @@
 // NodeList objects are collections of nodes, usually returned by properties such as Node.childNodes and methods such as document.querySelectorAll().
 
 // Node tree https://dom.spec.whatwg.org/#concept-node
+
+let container = document.querySelector(".container");
+console.log(container);
+
+const spanList = document.querySelectorAll(".container span");
+console.log(spanList);
+spanList.forEach((span) => {
+  span.innerHTML = "Nice 😇 <br>";
+  span.style.backgroundColor = "orange";
+});
+
+console.log(spanList.length);
+spanList[2].style.backgroundColor = "red";
+
+const menu = document.querySelector("#menuList");
+console.log(menu);
+
+// const highlight = document.querySelector(".highlight");
+// highlight.style.backgroundColor = "yellow";
+
+// + >
+const highlight = document.querySelectorAll("li[class='highlight']");
+console.log(highlight);
+highlight[0].style.backgroundColor = "blue";
+
+const moreClass = document.querySelector(".main");
+moreClass.classList.add("text");
+moreClass.classList.add("small");
+moreClass.classList.add("cool", "flex-size");
+console.log(moreClass.classList);
+moreClass.classList.remove("flex-size");
+console.log(moreClass.classList);
