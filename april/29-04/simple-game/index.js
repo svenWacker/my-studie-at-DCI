3f2216181edae664f3429956f4ec99c86017ac92
svenWacker <s.u.wacker@outlook.de>
2021-04-29 13:13:03 +0200

    simple game

--- april/29-04/simple-game/index.js
+++ april/29-04/simple-game/index.js
@@ -1,2 +1,20 @@
 // What is Q key code? so here all the keys code you need to know
 // https://keycode.info/
+let hStandPoint = 0;
+let vStandPoint = 0;
+function goUp() {
+  hStandPoint += 10;
+  mini.style.top = hStandPoint + "px";
+}
+function goDown() {
+  hStandPoint -= 10;
+  mini.style.top = hStandPoint + "px";
+}
+function goRight() {
+  vStandPoint += 10;
+  mini.style.left = vStandPoint + "px";
+}
+function goLeft() {
+  vStandPoint -= 10;
+  mini.style.left = vStandPoint + "px";
+}
