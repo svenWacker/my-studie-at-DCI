721ba00b61ca5ae3a3c2f58fe8f291f2020d8ec1
svenWacker <s.u.wacker@outlook.de>
2021-04-29 13:46:28 +0200

    simple game

--- april/29-04/simple-game/index.js
+++ april/29-04/simple-game/index.js
@@ -3,18 +3,35 @@
 let hStandPoint = 0;
 let vStandPoint = 0;
 function goUp() {
-  hStandPoint += 10;
-  mini.style.top = hStandPoint + "px";
+  vStandPoint -= 10;
+  mini.style.top = vStandPoint + "px";
 }
 function goDown() {
-  hStandPoint -= 10;
-  mini.style.top = hStandPoint + "px";
+  vStandPoint += 10;
+  mini.style.top = vStandPoint + "px";
 }
 function goRight() {
-  vStandPoint += 10;
-  mini.style.left = vStandPoint + "px";
+  hStandPoint += 10;
+  mini.style.left = hStandPoint + "px";
 }
 function goLeft() {
-  vStandPoint -= 10;
-  mini.style.left = vStandPoint + "px";
+  hStandPoint -= 10;
+  mini.style.left = hStandPoint + "px";
 }
+function move(e) {
+  console.log(e);
+  if (e.code == "KeyW") goUp();
+  if (e.code == "KeyA") goLeft();
+  if (e.code == "KeyD") goRight();
+  if (e.code == "KeyS") goDown();
+  console.log(hStandPoint);
+  console.log(vStandPoint);
+  if (
+    window.innerHeight / 4 <= vStandPoint &&
+    window.innerWidth - 70 <= hStandPoint
+  )
+    alert("YOU WIN");
+}
+const mini = document.querySelector(".mini");
+const body = document.querySelector("body");
+body.addEventListener("keypress", move);
