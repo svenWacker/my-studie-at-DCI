dbff07e9b1815ce1b2923e391a344e5e3e4439b2
svenWacker <s.u.wacker@outlook.de>
2021-05-04 09:58:23 +0200

    DOM and Apps

--- may/04-05/index.js
+++ may/04-05/index.js
@@ -1,13 +1,56 @@
 const result = document.querySelector(".result");
 const form = document.querySelector("form");
 const header = document.querySelector("h1");
-
+const msg = document.querySelector(".msg");
+const done = document.querySelector(".done");
+const changeColor = document.querySelector(".changeColor");
 // showMsg
 function newUser() {
-  result.innerHTML += "Hey, welcome again 😇";
+  result.innerHTML += "Hey, welcome again 😎 <br>";
 }
 function winDowLoad() {
   //alert("Hi");
-  setInterval(newUser, 1000);
+  //setInterval(newUser, 1000);
 }
+function redBg() {
+  this.style.backgroundColor = "red";
+}
+
+function whiteBg() {
+  this.style.backgroundColor = "white";
+}
+const start = () => {
+  header.addEventListener("mouseenter", redBg);
+  header.addEventListener("mouseleave", whiteBg);
+};
+
+const stop = () => {
+  header.removeEventListener("mouseenter", redBg);
+  header.removeEventListener("mouseleave", whiteBg);
+};
+// msg
+function showMsg(str) {
+  done.style.display = "block";
+  msg.innerHTML = str;
+}
+function hide() {
+  const close = document.querySelector(".close");
+
+  close.parentElement.style.display = "none";
+}
+
 window.addEventListener("load", winDowLoad);
+form.addEventListener("submit", (e) => {
+  // only for this week
+  e.preventDefault();
+  const userInput = document.querySelector("#userData").value;
+  showMsg(`Hey ${userInput}, thanks for your feedback`);
+});
+changeColor.addEventListener("click", (e) => {
+  e.preventDefault();
+  // do something cool :)
+  form.style.backgroundColor = "orange";
+});
+form.addEventListener("reset", () => {
+  showMsg("Your input has been cleared put.");
+});
