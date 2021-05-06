e37117b0e0e1cb9da9c84604bca12eadd2581e94
svenWacker <s.u.wacker@outlook.de>
2021-05-06 12:49:10 +0200

    Promises, blocking/non-blocking

--- may/06-05/index.js
+++ may/06-05/index.js
@@ -17,6 +17,45 @@ console.log(x);
 
 // Promises
 // Promises are very similar to callbacks but they are a little bit clean on writing
-// Promises are the best solution if you want to do something that takes a long time to be done and you don't want your app to wait for it to be done, so it will work on the background, Like downloading an image or a video from another server
+
+// Promises are the best solution if you want to do something that takes a long time to be done
+// and you don't want your app to wait for it to be done, so it will work on the background,
+// Like downloading an image or a video from another server
+
+const checkPromise = new Promise((resolve, reject) => {
+  //   let x = true;
+  let x = false;
+  if (x) {
+    resolve(`everything is cool.`);
+  } else {
+    reject(`sorry, it's not cool.`);
+  }
+});
+
+checkPromise
+  .then((message) => {
+    console.log(`I am then and ${message}`);
+  })
+  // more then are possible
+  //   .then((obj) => {
+  //     console.log(`step 2 ${obj}`);
+  //   })
+  .catch((err) => {
+    console.log(`I am catch and ${err}`);
+  });
 
 // Promisify setTimeout
+
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+let ms = 3000;
+function doSomething() {
+  let text = document.querySelector(".text");
+  text.innerHTML = `it took me ${ms}ms to be done.`;
+}
+delay(ms).then(doSomething);
