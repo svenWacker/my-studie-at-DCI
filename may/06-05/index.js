49eed95dd447e6ae87423179330ced8f5aa71c11
svenWacker <s.u.wacker@outlook.de>
2021-05-06 09:18:43 +0200

    Promises, blocking/non-blocking

--- may/06-05/index.js
+++ may/06-05/index.js
@@ -1,15 +1,20 @@
 // Blocking vs Non-Blocking code
-// Blocking code
-// blocking code(loops, console.log, loading files )
-// Non-blocking code
 
+// blocking code(loops, console.log, loading files )
+
+// Blocking code
 console.log("Hi");
 console.log("cool");
 
+// Non-blocking code
 window.setTimeout(() => {
   console.log("Hi there this is a time out");
 }, 1000);
 console.log("I can run first, even I am after the time out");
+
+let x = 33;
+console.log(x);
+
 // Promises
 // Promises are very similar to callbacks but they are a little bit clean on writing
 // Promises are the best solution if you want to do something that takes a long time to be done and you don't want your app to wait for it to be done, so it will work on the background, Like downloading an image or a video from another server
