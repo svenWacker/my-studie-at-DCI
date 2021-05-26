c5126eb54658d554728170b4259409854a550afc
svenWacker <s.u.wacker@outlook.de>
2021-05-26 09:44:11 +0200

    props and props drilling

--- may/26-05/src/App.js
+++ may/26-05/src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { Text, Hallo } from "./components/Text";
 
+function App() {
+  return (
+    <div>
+      <h1>Hi I am React</h1>
+      <Hallo />
+      <Text />
+      <Text />
+      <Text />
+    </div>
+  );
+}
 export default App;
---
+++ may/26-05/src/components/Text.js
@@ -0,0 +1,9 @@
+function Text() {
+  return <div>Hey I am component</div>;
+}
+function Hallo() {
+  return <div>Welcome to React</div>;
+}
+
+// export default Text;
+export { Text, Hallo };
