7234cd9013ec348268ed0038448737c47e3b6d6e
svenWacker <s.u.wacker@outlook.de>
2021-05-27 09:29:49 +0200

    insert de- and increment

--- may/26-05/src/App.js
+++ may/26-05/src/App.js
@@ -14,6 +14,34 @@ function App() {
       <Text />
       <ul>{listItems}</ul>
     </div>
+
+    //   <ul>
+    //names.map((name, i) => (
+    //   <li key={i}>{name}</li>
+    // ))
+    //  </ul>
   );
 }
 export default App;
+
+// Increments app
+/*
+import { useState } from "react";
+function App() {
+const [clicks, setClicks] = useState(0);
+function increment() {
+setClicks(clicks + 1);
+}
+function decrement() {
+setClicks((prevState) => prevState - 1);
+}
+return (
+<div>
+You click {clicks} times!
+<br />
+<button onClick={increment}>+</button>
+<button onClick={decrement}>-</button>
+</div>
+);
+}
+*/
