38e92b69e759dff2c351ab2d327ffd46e9720675
svenWacker <s.u.wacker@outlook.de>
2021-07-12 09:25:23 +0200

    redux day 1

---
+++ july/redux-simplified/src/actions/index.js
@@ -0,0 +1,13 @@
+// step 1
+const increment = () => {
+  return { type: "INCREMENT" };
+};
+
+const decrement = () => {
+  return { type: "DECREMENT" };
+};
+const changeName = (name) => {
+  return { type: "CHANGE_NAME", payload: name };
+};
+
+export { increment, decrement, changeName };
