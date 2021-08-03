14d7e9f3fef8f6057efa1386ed87f95fbc301352
svenWacker <s.u.wacker@outlook.de>
2021-08-03 09:48:33 +0200

    backend file sys and args

---
+++ august/03-08/index.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+// create file
+// writeFile will create or over write
+
+// appendFile will create file
+fs.appendFile("newText.txt", "I am text from NodeJs 😎 \n", (err) => {
+  if (err) throw err;
+  console.log("File is done 👍🏻");
+});
+
+// read File
+fs.readFile("./longText.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+// read file Sync
+const data = fs.readFileSync("./longText.txt");
+console.log(data.toString());
+console.log("reading sync Done ✅");
+
+// rename file
+// fs.rename("./hello.txt", "./hey.txt", (err) => {
+//   if (err) throw err;
+//   console.log("Rename done ✅");
+// });
+
+// delete file
+// const filePathName = "./something.txt";
+// fs.unlink(filePathName, (err) => {
+//   if (err) throw err;
+//   console.log("File deleted, done ✅");
+// });
+
+// copy file
+fs.copyFile("./original.txt", "./copyFromOG.txt", (err) => {
+  if (err) throw err;
+  console.log("Copy file, Done ✅");
+});
+
+// open file
+// fs flags
+// https://nodejs.org/docs/latest-v12.x/api/fs.html#fs_file_system_flags
+fs.open("newText.txt", "w", (err, data) => {
+  if (err) throw err;
+  console.log("File is opened, Done ✅");
+});
