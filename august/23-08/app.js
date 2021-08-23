b7e64e09bee2ed3ade55e7f1323033b84ca43875
svenWacker <s.u.wacker@outlook.de>
2021-08-23 09:44:58 +0200

    mongoDB - server connected

--- august/23-08/app.js
+++ august/23-08/app.js
@@ -6,4 +6,12 @@ app.use(morgan("dev"));
 // to process the json data
 app.use(express.json());
 
+// mongoDB
+const mongoose = require("mongoose");
+const DB_URL = process.env.DB_URL;
+mongoose
+  .connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(console.log("DB is connected"))
+  .catch((error) => console.log("There is a problem"));
+
 module.exports = app;
