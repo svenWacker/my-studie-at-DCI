0fa818a59b97692a30c454db507285165d097f4f
svenWacker <s.u.wacker@outlook.de>
2021-08-25 10:23:42 +0200

    updating organized RESTAPI

--- august/25-08/controllers/employeeController.js
+++ august/25-08/controllers/employeeController.js
@@ -2,7 +2,7 @@ const EmployeesData = require("../model/employeesModel");
 const express = require("express");
 
 // Middleware
-// only one
+// Get one employee by name (only one)
 const getEmployee = async (req, res, next) => {
   let employee;
   try {
@@ -13,6 +13,23 @@ const getEmployee = async (req, res, next) => {
     if (employee == null) {
       // NOt found
       return res.status(404).json({ message: "Sorry, employee NOT FOUND." });
+    }
+  } catch (err) {
+    // 500 Internal server error
+    res.status(500).json({ message: err.message });
+  }
+  res.employee = employee;
+  next();
+};
+// Get one add
+const getAdd = async (req, res, next) => {
+  // find will get all or with criteria wil get some
+  let employee;
+  try {
+    employee = await EmployeesData.find({ add: req.params.add });
+    if (employee == null) {
+      // NOt found
+      return res.status(404).json({ message: "Sorry, NOT FOUND." });
     }
   } catch (err) {
     // 500 Internal server error
