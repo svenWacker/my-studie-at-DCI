8f75b2cd1af71b21960282731ea18f1226e17237
svenWacker <s.u.wacker@outlook.de>
2021-08-30 14:14:59 +0200

    full app

--- august/30-08/app.js
+++ august/30-08/app.js
@@ -5,8 +5,9 @@ app.use(morgan("dev"));
 
 const mongoose = require("mongoose");
 const faker = require("faker");
+// Get you the
 const path = require("path");
-//const fakeModel = require("./model/user");
+const fakeModel = require("./model/user");
 mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
@@ -16,5 +17,40 @@ mongoose
   .catch((err) => {
     console.log(`There was error ${err.message}`);
   });
+// console.log(path.resolve(__dirname, "views"));
+// console.log(faker.animal.dog());
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  // res.render("home", { message: "Test" });
+  fakeModel.find((err, data) => {
+    if (err) {
+      console.log(err);
+    } else if (data) {
+      // res.render("home",{data:data})
+      res.render("home", { data });
+    } else {
+      res.render("home", { data: {} });
+    }
+  });
+});
+app.post("/", (req, res) => {
+  for (let i = 0; i < 10; i++) {
+    const fakeData = new fakeModel({
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      city: faker.address.city(),
+      imageUrl: faker.image.imageUrl(),
+    });
+    try {
+      fakeData.save();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  res.redirect("/");
+});
 
 module.exports = app;
