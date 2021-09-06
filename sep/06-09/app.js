da5cd54890862adf32ad7494091959f166d38063
svenWacker <s.u.wacker@outlook.de>
2021-09-06 10:43:32 +0200

    validation and hbs

---
+++ sep/06-09/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+const hbs = require("express-handlebars");
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defaultLayout: "layout",
+    layoutsDir: __dirname + "/views/layouts/",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Validation",
+    data: "Hey welcome to our app 😎 ",
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    data: "This is us 🤓 ☺️ 😎 ",
+  });
+});
+
+app.post("/submit", (req, res) => {
+  // req has form data
+});
+module.exports = app;
